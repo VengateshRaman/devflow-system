31e6487f5c6a499f0226073ddb7544c1ca72c99d
Vengatesh Raman <r.rvengatesh@gmail.com>
2026-03-05 07:53:42 +0530

    basic installation

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -1,34 +1,28 @@
 "use client";
+import { memo, useState } from "react";
 
-import { useState, useEffect } from "react";
+const Child = memo(function Child() {
+  console.log("Child Rendered");
+  return <div className="mt-4">I am child</div>;
+});
 
 export default function Home() {
-  console.log("component render");
+  console.log("Parent Rendered");
 
   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  useEffect(() => {
-    console.log("effect mounted");
-
-    return () => {
-      console.log("cleanup");
-    };
-  }, []);
-
   return (
     <main className="p-10">
-      <h1 className="text-2xl font-bold">Render Test</h1>
+      <h1 className="text-2xl font-bold">Parent → Child Test</h1>
       <p className="mt-4">Count: {count}</p>
       <button
-        onClick={increment}
+        onClick={() => setCount(count + 1)}
         className="mt-4 bg-black text-white px-4 py-2 rounded"
       >
         Increment
       </button>
+
+      <Child />
     </main>
   );
 }
